3cf947bfb281f70ab1f32fde5b2c5883ba0f42ed
Azirielle <andrews.adarayan@gmail.com>
2026-01-17 16:04:14 +0800

    23456yujhappserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Force set Cloudinary config at runtime
+        config([
+            'cloudinary.cloud_url' => env('CLOUDINARY_URL'),
+            'cloudinary.upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+            'cloudinary.notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+            'cloudinary.secure' => true,
+        ]);
     }
 
     /**
