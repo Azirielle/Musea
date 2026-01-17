8efe912ab1f01704689914fd850d4aceab343411
Azirielle <andrews.adarayan@gmail.com>
2026-01-17 14:26:15 +0800

    truekeks

--- app/Http/Controllers/Admin/ArtworkController.php
+++ app/Http/Controllers/Admin/ArtworkController.php
@@ -14,6 +14,7 @@ class ArtworkController extends Controller
         $pendingArtworks = Artwork::where('status', 'pending')
             ->with('artist:id,first_name,last_name')
             ->latest()
+            ->get()
             ->map(function ($artwork) {
                 return [
                     'id' => $artwork->id,
